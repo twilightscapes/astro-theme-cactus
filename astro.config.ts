347a56b61f5c0737bdd15279b214533fd28d4be7
Todd <ihatespam@gmail.com>
2023-09-13 06:23:32 -0500

    added cms

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import NetlifyCMS from 'astro-netlify-cms';
 import fs from 'fs';
+import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 import remarkUnwrapImages from "remark-unwrap-images";
-import react from '@astrojs/react';
 // @ts-ignore:next-line
 import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
 
@@ -29,6 +30,53 @@ export default defineConfig({
 		sitemap(),
 		prefetch(),
 		react(),
+		NetlifyCMS({
+			config: {
+				// Use Netlify’s “Git Gateway” authentication and target our default branch
+				backend: {
+					name: 'git-gateway',
+					branch: 'latest',
+				},
+				// Configure where our media assets are stored & served from
+				media_folder: 'public/assets/blog',
+				public_folder: '/assets/blog',
+				// Configure the content collections
+				collections: [
+					{
+						name: 'posts',
+						label: 'Blog Posts',
+						label_singular: 'Blog Post',
+						folder: 'src/pages/posts',
+						create: true,
+						delete: true,
+						fields: [
+							{ name: 'title', widget: 'string', label: 'Post Title' },
+							{
+								name: 'publishDate',
+								widget: 'datetime',
+								format: 'DD MMM YYYY',
+								date_format: 'DD MMM YYYY',
+								time_format: false,
+								label: 'Publish Date',
+							},
+							{ name: 'author', widget: 'string', label: 'Author Name', required: false },
+							{ name: 'authorURL', widget: 'string', label: 'Author URL', required: false },
+							{ name: 'description', widget: 'string', label: 'Description', required: false },
+							{ name: 'body', widget: 'markdown', label: 'Post Body' },
+							{
+								name: 'layout',
+								widget: 'select',
+								default: '../../layouts/BlogPost.astro',
+								options: [
+									{ label: 'Blog Post', value: '../../layouts/BlogPost.astro' },
+								],
+							},
+						],
+					},
+				],
+			},
+			previewStyles: ['/src/styles/blog.css'],
+		}),
 	],
 	vite: {
 		plugins: [rawFonts(['.ttf'])],
