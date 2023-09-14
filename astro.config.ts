96a85253f6d3f42e9ea8b4da251bd44a7f29aa61
Todd <ihatespam@gmail.com>
2023-09-14 05:56:08 -0500

    cms but switch no worky

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import NetlifyCMS from 'astro-netlify-cms';
 import fs from 'fs';
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
