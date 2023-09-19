77843c6f96b72bad728f95917511ebb219fa1f94
Todd <ihatespam@gmail.com>
2023-09-19 06:35:01 -0500

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -98,7 +98,26 @@ export default defineConfig({
 		},
 		plugins: [rawFonts(['.ttf']), VitePWA({
 			registerType: "autoUpdate",
-			manifest,
+			injectRegister: 'auto',
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+			manifest: {
+				name: 'My Awesome App',
+				short_name: 'MyApp',
+				description: 'My Awesome App description',
+				theme_color: '#ffffff',
+				icons: [
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			},
 			workbox: {
 				globDirectory: 'dist',
 				globPatterns: [
